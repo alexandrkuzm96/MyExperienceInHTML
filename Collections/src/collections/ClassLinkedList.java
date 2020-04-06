4ee6ae90876d37cc39a6f18272081bac75dd2a4b
Alexandr <alexandrkuzm96@gmail.com>
2020-04-06 12:03:55 +0300

    I ere created linked list and something operation with it.

--- Collections/src/collections/ClassLinkedList.java
+++ Collections/src/collections/ClassLinkedList.java
@@ -1,6 +1,25 @@
 package collections;
 
-import java.util.LinkedHashMap;
+import java.util.LinkedList;
+
 public class ClassLinkedList {
+    public void ArrayOperation(){
+        LinkedList<String> array1=new LinkedList<String>();
+        LinkedList<String> array2=new LinkedList<String>();
+        array1.add("Moscov");
+        array1.addFirst("Vladimir");
+        array1.add(1, "Rostov");
+        System.out.println("Array 1 is empty "+array1.isEmpty());
+        System.out.println("Indexs of Rostov "+array1.indexOf("Rostov"));
+        
+        array2.offer("Gus");
+        array2.offerFirst("Ekarelinburg");
+        array1.addAll(1, array2);
+        System.out.println("array1 contains array2 "+array1.containsAll(array2));
+                
+        for(String s: array1){
+            System.out.println(s);
+        }
+    }
     
 }
--- Collections/src/collections/Collections.java
+++ Collections/src/collections/Collections.java
@@ -3,10 +3,15 @@ package collections;
 public class Collections {
 
     public static void main(String[] args) {
+        System.out.println("Array list operations");        
         ClassArrayList  arrayList=new ClassArrayList ();
         arrayList.ListOperation();
+        System.out.println("Array deque operations");
         ClassArrayDeque arrayDeque=new ClassArrayDeque();
         arrayDeque.ArrayOperation();
+        System.out.println("LinkeList operations");
+        ClassLinkedList linkedList=new ClassLinkedList();
+        linkedList.ArrayOperation();
     }
     
 }
